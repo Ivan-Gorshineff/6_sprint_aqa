6776d922d3f10b5db5ca62c535df31c412c9b322
Ivan <ivangorshinef@gmail>
2025-03-24 09:19:11 +0300

    добавил urls

---
+++ Urls.py
@@ -0,0 +1,6 @@
+class Urls:
+
+    BASE_URL = 'https://qa-scooter.praktikum-services.ru'
+    MAIN_PAGE_URL = f'{BASE_URL}/'
+    ORDER_PAGE_URL = f'{BASE_URL}/order'
+    REDIRECT_URL = 'https://dzen.ru/?yredirect=true'
