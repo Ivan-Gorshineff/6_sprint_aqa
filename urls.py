b8ff04dca0c51da046f7c2abd10e44475bef3a3f
Ivan <ivangorshinef@gmail>
2025-03-24 16:55:15 +0300

    изменил урUrls

--- urls.py
+++ urls.py
@@ -3,4 +3,4 @@ class Urls:
     BASE_URL = 'https://qa-scooter.praktikum-services.ru'
     MAIN_PAGE_URL = f'{BASE_URL}/'
     ORDER_PAGE_URL = f'{BASE_URL}/order'
-    REDIRECT_URL = 'https://dzen.ru/?yredirect=true'
+    DZEN_URL = 'https://dzen.ru/?yredirect=true'
