635917dcbcdfa65e8ab849d6efe116ac4ce272b1
Ivan <ivangorshinef@gmail>
2025-03-24 17:28:24 +0300

    добавил список Ðизменил урлы

--- urls.py
+++ urls.py
@@ -3,4 +3,4 @@ class Urls:
     BASE_URL = 'https://qa-scooter.praktikum-services.ru'
     MAIN_PAGE_URL = f'{BASE_URL}/'
     ORDER_PAGE_URL = f'{BASE_URL}/order'
-    DZEN_URL = 'https://dzen.ru/?yredirect=true'
+    REDIRECT_URL = 'https://dzen.ru/?yredirect=true'
